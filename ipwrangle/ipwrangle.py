27d050d2378246f533c0059d0081ef8ed4525ee0
Yonathan Klijnsma <admin@0x3a.com>
2020-03-29 17:31:27 -0600

    Fix broken pipe issue
    Piping output from ipwrangle to tools like head/tail would result in:
    
    ```Traceback (most recent call last):
      File "/usr/local/bin/ipexpand", line 8, in <module>
        sys.exit(expand_main())
      File "/usr/local/lib/python3.7/site-packages/ipwrangle/ipwrangle.py", line 24, in expand_main
        [print(ip) for ip in IPNetwork(line)]
      File "/usr/local/lib/python3.7/site-packages/ipwrangle/ipwrangle.py", line 24, in <listcomp>
        [print(ip) for ip in IPNetwork(line)]
    BrokenPipeError: [Errno 32] Broken pipe
    Exception ignored in: <_io.TextIOWrapper name='<stdout>' mode='w' encoding='UTF-8'>
    BrokenPipeError: [Errno 32] Broken pipe```
    
    Updated version no longer does this.

--- ipwrangle/ipwrangle.py
+++ ipwrangle/ipwrangle.py
@@ -5,6 +5,27 @@ import argparse
 
 import ipaddress
 from netaddr import IPNetwork
+from signal import signal, SIGPIPE, SIG_DFL
+
+# http://newbebweb.blogspot.com/2012/02/python-head-ioerror-errno-32-broken.html
+signal(SIGPIPE,SIG_DFL)
+
+# Taken from: https://mail.python.org/pipermail/tutor/2003-November/026645.html
+class Unbuffered(object):
+   def __init__(self, stream):
+       self.stream = stream
+   def write(self, data):
+       self.stream.write(data)
+       self.stream.flush()
+   def writelines(self, datas):
+       self.stream.writelines(datas)
+       self.stream.flush()
+   def __getattr__(self, attr):
+       return getattr(self.stream, attr)
+
+
+# Remove buffering, processing large datasets eats memory otherwise
+sys.stdout = Unbuffered(sys.stdout)
 
 def parse_args(prog):
     parser = argparse.ArgumentParser(prog=prog)
@@ -24,12 +45,15 @@ def expand_main():
         [print(ip) for ip in IPNetwork(line)]
 
 def reduce_main():
-    args = parse_args('ipreduce')
+    try:
+        args = parse_args('ipreduce')
 
-    worklist = []
-    if args.value:
-        worklist = [line.rstrip() for line in args.value.split(',')]
-    else:
-        worklist = [line.rstrip() for line in sys.stdin.readlines()]
+        worklist = []
+        if args.value:
+            worklist = [line.rstrip() for line in args.value.split(',')]
+        else:
+            worklist = [line.rstrip() for line in sys.stdin.readlines()]
 
-    [print(cidr) for cidr in list(ipaddress.collapse_addresses([ipaddress.ip_network(ip) for ip in sorted(worklist)]))]
+        [print(cidr) for cidr in list(ipaddress.collapse_addresses([ipaddress.ip_network(ip) for ip in sorted(worklist)]))]
+    except KeyboardInterrupt:
+        sys.stdout.write('\n')
